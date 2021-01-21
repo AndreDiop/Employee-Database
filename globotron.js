c1d7ca95ce036a4f53bb1f662216869ad51c4c3a
Andre Diop <itsjustandre@gmail.com>
2021-01-21 18:08:09 -0500

    Installed Dependancies
    - Added Inquirer
    - Added mysql package
    - created connection to 'globotron' database

---
+++ globotron.js
@@ -0,0 +1,25 @@
+const mysql = require("mysql");
+// const inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "trapstar",
+  database: "globotron",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+});
+
+function exit() {
+  connection.end();
+}
